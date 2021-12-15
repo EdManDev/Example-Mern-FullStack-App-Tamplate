0b4015efcbff2ab35c37291d37c7422152adee51
EdManDev <ed.manigat@gmail.com>
2021-12-15 13:18:22 -0400

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,5 @@
+const express = require("express");
+
 // ======================================================================================
 // @desc      Response User
 // @route     GET /api/user
